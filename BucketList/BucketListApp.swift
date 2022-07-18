ec592edfea7df660d80f124248f02b883eb6c288
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2022-07-18 19:23:25 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jordan Haynes on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
