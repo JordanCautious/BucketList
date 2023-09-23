b58c67ee24bf5adca31a63686b10b0b0adb36241
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-23 03:14:48 -0400

    Added minor changes

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -14,7 +14,18 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                MapAnnotation(coordinate: location.coordinate) {
+                    VStack {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.mint)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(Circle())
+                        
+                        Text(location.name)
+                    }
+                }
             }
                 .ignoresSafeArea()
             
