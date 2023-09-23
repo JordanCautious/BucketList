2773ec70aea13cdcc4660f1aa39216db67cfe46c
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-23 00:45:34 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jordan Haynes on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Jordan Haynes on 9/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
