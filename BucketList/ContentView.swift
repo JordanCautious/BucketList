0cf58d25ae641f3cf757c3c456fffc5e5dca43e2
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-23 02:16:07 -0400

    Beginning of BucketList. Trying to figure out what looks best and how MapKit works. Some parts were deprecated for iOS 17 so will have to redo those sections

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,38 @@
 //  Created by Jordan Haynes on 9/23/23.
 //
 
+import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+            .navigationTitle("London Explorer")
         }
-        .padding()
     }
 }
 
