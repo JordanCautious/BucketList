4e9ddb54dca2729f2dd2f3344b9f37fd5d22bb05
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-24 04:31:13 -0400

    Added an edit view to the application

---
+++ BucketList/EditView.swift
@@ -0,0 +1,53 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Jordan Haynes on 9/24/23.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    @Environment(\.dismiss) var dismiss
+    var location: Location
+    var onSave: (Location) -> Void
+    
+    @State private var name: String
+    @State private var description: String
+    
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Place name", text: $name)
+                    TextField("Description", text: $description)
+                    
+                }
+            }
+            .navigationTitle("Place details")
+            .toolbar {
+                Button("Save") {
+                    var newLocation = location
+                    newLocation.id = UUID()
+                    newLocation.name = name
+                    newLocation.description = description
+                    
+                    onSave(newLocation)
+                    dismiss()
+                }
+            }
+        }
+    }
+    
+    init(location: Location, onSave: @escaping (Location) -> Void) {
+        self.location = location
+        self.onSave = onSave
+        _name = State(initialValue: location.name)
+        _description = State(initialValue: location.description)
+    }
+}
+
+#Preview {
+    EditView(location: Location.example) { _ in }
+}
